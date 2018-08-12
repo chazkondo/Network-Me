c67491a5a504b3ffce6f65591c448815680c1aec
Mckarl Manuel <manmckarl@gmail.com>
2018-08-11 17:58:33 -1000

    converted html to handlebars, moved js/css/fonts/images folders to public, GET to login/home/register functionality

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 const express = require('express');
-
+const handlebars = require('express-handlebars');
 
 const PORT = process.env.PORT || 4020;
 const server = express();
 
 server.use(express.static('./public'));
 
-server.get('/', (req, res)=>{
-  res.send('wiggity woo');
-})
+server.get('/', (req, res) => {
+  res.render('pages/home')
+});
+
+server.engine('.hbs', handlebars({
+  defaultLayout: 'index',
+  extname: '.hbs'
+}));
+
+server.set('view engine', '.hbs');
+
+// GET
+server.get('/', (req, res) => {
+  res.render('pages/home')
+});
+
+server.get('/login', (req, res)=>{
+  res.render('pages/login')
+});
+
+server.get('/register', (req, res)=>{
+  res.render('pages/register');
+});
+
+// POST
+// server.post('/login')
+
+// CATCH ALL
+server.get('*', (req, res) => {
+  res.send('Bad URL.');
+});
 
 server.listen(PORT, () => {
   console.log(`Connected to ${PORT}`)
-})
+});
